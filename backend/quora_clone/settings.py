44b56c48a971c1bb738170dd38a9aa7740121c76
Mohit Lahoti <engtechno25@gmail.com>
2025-04-09 01:06:06 +0530

    Add static route to settings.py

--- backend/quora_clone/settings.py
+++ backend/quora_clone/settings.py
@@ -121,7 +121,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
 
 MEDIA_URL = '/media/'
 
